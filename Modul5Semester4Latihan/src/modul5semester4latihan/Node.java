bc2ab44cadc54a09c09cb25e5d231dd397aa62c5
SAliHisyam <127366847+SAliHisyam@users.noreply.github.com>
2023-06-17 17:21:02 +0700

    Add files via upload

---
+++ Modul5Semester4Latihan/src/modul5semester4latihan/Node.java
@@ -0,0 +1,15 @@
+package modul5semester4latihan;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Node {
+    int data;
+    Node left;
+    Node right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
